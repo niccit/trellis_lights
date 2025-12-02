de3312fadda71690c49f2cdab3089c3324fd2f16
niccit <ntynen+github@gmail.com>
2025-12-01 16:30:06 -1000

    shortened data.py length, the full version was causing memory errors. addressed handling issue when changing booleans in the data file if mqtt loop can't start disconnect to force reconnect logic allowed code to continue if can't connect to broker discovered 0 can be sent as a number via MQTT so changed handling in variable assignments

--- code.py
+++ code.py
@@ -1,4 +1,6 @@
 # SPDX-License-Identifier: MIT
+import gc
+
 import board
 import neopixel
 import json
@@ -10,11 +12,19 @@ import adafruit_logging
 import adafruit_connection_manager
 import adafruit_minimqtt.adafruit_minimqtt
 from adafruit_minimqtt.adafruit_minimqtt import MMQTTException
+from adafruit_minimqtt.adafruit_minimqtt import MMQTTStateError
 from adafruit_led_animation.group import AnimationGroup
 from adafruit_led_animation.sequence import AnimationSequence
-from circuitpy_helpers.led_animations import animationbuilder
+from circuitpy_helpers.led_animations import animationBuilder
 from circuitpy_helpers.file_helpers import updateFiles
-from circuitpy_helpers.color_helpers import getColors
+from circuitpy_helpers.led_animations import updateAnimationData
+from circuitpy_helpers.calendar_time_helpers import timeHelper
+from circuitpy_helpers.led_animations import controlLights
+from circuitpy_helpers.network_helpers import wanChecker
+
+# --- Logging --- #
+logger = adafruit_logging.getLogger("trellis_lights")
+logger.setLevel(adafruit_logging.INFO)
 
 # --- Data imports --- #
 try:
@@ -23,15 +33,27 @@ except ImportError as e:
     print(f"unable to import data: {e}")
     raise
 
-# --- Logging --- #
-logger = adafruit_logging.getLogger("trellis_lights")
-logger.setLevel(adafruit_logging.INFO)
-
-# --- Pixels Configuration --- #
+# Variable assignments
 max_brightness = data["brightness_high"]
 min_brightness = data["brightness_low"]
 num_pixels = data["num_pixels"]
 reset_wait = data['reset_lighting_timeout']
+before_sunset = data["seconds_before_sunset"]
+sleep_time = data["sleep_time"]
+if data["stop_time"] != 0:
+    stop_time = timeHelper.get_time_in_seconds(data["stop_time"])
+else:
+    stop_time = int(data["stop_time"])
+ignore_sunset_string = (data["ignore_sunset"])
+if ignore_sunset_string is "False":
+    ignore_sunset = False
+else:
+    ignore_sunset = True
+running = False
+time_in_seconds = None
+sunset_in_seconds = None
+
+# --- Pixels Configuration --- #
 pixels_0 = neopixel.NeoPixel(board.SIG3, num_pixels, auto_write=False, pixel_order=neopixel.RGB)
 pixels_0.brightness = max_brightness
 pixels_1 = neopixel.NeoPixel(board.SIG2, num_pixels, auto_write=False, pixel_order=neopixel.RGB)
@@ -40,7 +62,7 @@ pixels_1.brightness = max_brightness
 # --- MQTT Configuration --- #
 radio = wifi.radio
 pool = adafruit_connection_manager.get_radio_socketpool(radio)
-ssl_context = adafruit_connection_manager.get_radio_socketpool(radio)
+ssl_context = adafruit_connection_manager.get_radio_ssl_context(radio)
 
 # MQTT Feeds
 subscribe_list = []
@@ -48,6 +70,10 @@ trellis_lights = os.getenv("mqtt_trellis_lights_feed")
 subscribe_list.append(trellis_lights)
 motion_detect = os.getenv("mqtt_motion_detect_feed")
 subscribe_list.append(motion_detect)
+date_time = os.getenv("mqtt_datetime_feed")
+subscribe_list.append(date_time)
+sunset = os.getenv("mqtt_sunset_feed")
+subscribe_list.append(sunset)
 
 # MQTT specific helpers
 def on_connect(mqtt_client, userdata, flags, rc):
@@ -63,7 +89,7 @@ def on_disconnect(mqtt_client, userdata, rc):
     # from the broker.
     logger.warning(f"{mqtt_client} Disconnected from MQTT Broker!")
     counter = 0
-    sleep_time = 1
+    backoff_sleep = 1
     backoff_increment = 1
     while counter <= 10:
         try:
@@ -72,10 +98,10 @@ def on_disconnect(mqtt_client, userdata, rc):
         except MMQTTException:
             counter += 1
             if counter - 1 == 0:
-                time.sleep(sleep_time)
+                time.sleep(backoff_sleep)
             else:
-                sleep_time += backoff_increment
-                time.sleep(sleep_time)
+                backoff_sleep += backoff_increment
+                time.sleep(backoff_sleep)
             pass
 
 def on_subscribe(mqtt_client, userdata, topic, granted_qos):
@@ -92,7 +118,7 @@ def on_publish(mqtt_client, userdata, topic, pid):
 
 need_reload = False
 def on_message(client, topic, message):
-    global need_reload
+    global need_reload, time_in_seconds, sunset_in_seconds
     logger.info(f"New message for {client} on topic {topic}: {message}")
     # Support changes to the light configurations in the data.py file
     if "trellis" in topic:
@@ -101,8 +127,11 @@ def on_message(client, topic, message):
         # it will be transformed to the actual value in the data file before calling updater.update_data_file
         search_string = received_message["search_string"]
         received_message['search_string'] = str(data[received_message["search_string"]])
+        logger.info(f"received message is {received_message}")
         updated_message = json.dumps(received_message)
+        logger.info(f"updated message is {updated_message}")
         updateFiles.update_data_file(updated_message, search_string)
+        time.sleep(1)
         supervisor.reload()
     if "motion" in topic:
         if "1" in message:
@@ -110,7 +139,32 @@ def on_message(client, topic, message):
             message = ({"filename": "data.py", "search_string": str(data['animations']), "new_value": "'motion_solid'"})
             updated_message = json.dumps(message)
             updateFiles.update_data_file(updated_message, "animations")
+            time.sleep(1)
             supervisor.reload()
+    if "time" in topic:
+        time_in_seconds = timeHelper.get_time_in_seconds(message)
+    if "sunset" in topic:
+        sunset_in_seconds = timeHelper.get_time_in_seconds(message)
+
+    # On start up see if it's before assigned start time and if it is sleep
+    # Set that the lights are running so that future checks will direct activity
+    #   to the shutdown routine instead
+    # blank all strands prior to sending shutdown or sleep calls
+    if time_in_seconds and sunset_in_seconds:
+        global running
+        if not running:
+            need_sleep = controlLights.check_need_sleep(time_in_seconds, sunset_in_seconds, before_sunset, ignore_sunset)
+            if need_sleep:
+                controlLights.blank_all(pixels_0)
+                controlLights.blank_all(pixels_1)
+                controlLights.sleep_before_set_time(time_in_seconds, sunset_in_seconds, before_sunset)
+            running = True
+        else:
+            need_shutdown = controlLights.check_need_shutdown(time_in_seconds, stop_time, sunset_in_seconds, sleep_time, before_sunset)
+            if need_shutdown:
+                controlLights.blank_all(pixels_0)
+                controlLights.blank_all(pixels_1)
+                controlLights.shutdown(time_in_seconds, sunset_in_seconds, sleep_time, before_sunset)
 
 mqtt_local_broker = os.getenv("mqtt_local_server")
 mqtt_local_port = os.getenv("mqtt_local_port")
@@ -135,60 +189,37 @@ local_mqtt.on_publish = on_publish
 local_mqtt.on_message = on_message
 
 # Connect
-try:
-    local_mqtt.connect()
-except adafruit_minimqtt.adafruit_minimqtt.MMQTTException:
-    logger.error("Failed to connect to MQTT broker!")
+network_status = wanChecker.cpy_wan_active()
+# Only try to connect if WAN is accessible
+if network_status:
+    try:
+        local_mqtt.connect()
+    except adafruit_minimqtt.adafruit_minimqtt.MMQTTException:
+        logger.error("Failed to connect to MQTT broker")
+        pass
+else:
+    logger.error("Network not connected, did not try to connect to MQTT broker!")
 
 # -- Build chosen animation objects -- #
-selected_animations = data["animations"]
-animation_builder = animationbuilder
+chosen_animations = data["animations"]
+animation_builder = animationBuilder
 animation_group = []
 color_choice = None
+override_array = ["sparkles", "speed", "rate", "count", "period", "tail_length", "step", "reverse", "spacing",
+                  "size", "bounce"]
 # Read in all animations from json file
 # And build the animation objects and append them to the array
 with open("circuitpy_helpers/led_animations/animations.json", "r") as infile:
     adata = json.load(infile)
     for item in adata['animations']:
-        if item['name'] in selected_animations:
+        if item['name'] in chosen_animations:
             # Check for any animation overrides and update the JSON object
-            try:
-                override_array = ["sparkles", "speed", "rate", "count", "period", "tail_length", "step", "reverse", "spacing", "size", "bounce"]
-                for o in override_array:
-                    name = item['name'] + "_" + o + "_override"
-                    if name in data:
-                        item[o] = data[name]
-            except KeyError as e:
-                pass
-            # Set the color based on what's specified in json file
-            # If sending an array of colors, animation must support it
-            try:
-                if "random" in item['colors']:
-                    color = getColors.get_random_color()
-                    item['colors'] = color
-                elif "data" in item['colors']:
-                    color_list = []
-                    name = item['name'] + "_color"
-                    colors = data[name]
-                    if type(colors) == list:
-                        for color in colors:
-                            choice = getColors.get_color_tuple(color)
-                            color_list.append(choice)
-                        item['colors'] = color_list
-                    elif "palette" in colors:
-                        header, palette = colors.split("_")
-                        color_list = getColors.get_color_palette(palette)
-                        item['colors'] = color_list
-                    else:
-                        choice = getColors.get_color_tuple(colors)
-                        item['colors'] = choice
-            except KeyError as e:
-                pass
-
-            logger.debug(f"item to send is {item}")
-
-            obj_0 = animation_builder.build_animation(pixels_0, item)
-            obj_1 = animation_builder.build_animation(pixels_1, item)
+            item_with_overrides = updateAnimationData.override_default_settings(data, override_array, item)
+            # Set the color choice
+            updated_item = updateAnimationData.set_color(data, item_with_overrides)
+            logger.info(f"item to send is {updated_item}")
+            obj_0 = animation_builder.build_animation(pixels_0, updated_item)
+            obj_1 = animation_builder.build_animation(pixels_1, updated_item)
             animation_group.append(obj_0)
             animation_group.append(obj_1)
 
@@ -215,15 +246,17 @@ FRAME_DELAY = 0.01    # 100 FPS (20 ms per frame)
 MQTT_POLL_EVERY = 100 # poll MQTT every 100 frames (~2 seconds at 50 FPS)
 frame_counter = 0
 
+# --- Main --- #
 logger.info(f"Trellis Lights starting up")
 while True:
-
     # start animations
     animations.animate()
 
     frame_counter += 1
-    if frame_counter >= MQTT_POLL_EVERY:
 
+    # Poll MQTT and do other tasks
+    # Will cause a brief pause in the running animation
+    if frame_counter >= MQTT_POLL_EVERY:
         # check to see if we need to reset back to normal operations
         current_animation = data['animations']
         if "motion_solid" in current_animation:
@@ -231,13 +264,21 @@ while True:
             time.sleep(reset_wait)
             updateFiles.backup_and_restore("/data.py", restore=True)
 
+        # check WAN state
+        wan_state = wanChecker.cpy_wan_active()
+
         # if MQTT_POLL_EVERY criterion is met, loop mqtt for 1 second
-        try:
-            local_mqtt.loop(timeout=1)
-        except OSError as e:
-            print("MQTT error:", e)
-            # optional reconnect logic here
-            # We're using the on_disconnect method
+        # Only attempt this if WAN is accessible:1
+        if wan_state:
+            try:
+                local_mqtt.loop(timeout=1)
+            except MMQTTException as e:
+                print("MQTT error:", e)
+                local_mqtt.disconnect()
+                # optional reconnect logic here
+                # We're using the on_disconnect method
+                pass
+
         frame_counter = 0
 
     time.sleep(FRAME_DELAY)
--- data.py
+++ data.py
@@ -3,18 +3,37 @@ data = {
     ,'num_pixels': 400
     ,'brightness_high': 0.2
     ,'brightness_low': 0.025
+    ,'animation_wait': 30
+    ,'reset_lighting_timeout': 10
+    ,'stop_time': '22:00'
+    ,'sleep_time': 86400
+    ,'ignore_sunset': 'False'
+    ,'seconds_before_sunset': 900
     ,'custom_breathe_color': ['blue', 'purple', 'indigo']
     ,'custom_sparkle_color': 'cyan'
-    ,'custom_blink_color': 'chocolate'
+    ,'custom_blink_color': 'None'
     ,'multi_comet_color': 'palette_christmas'
+    ,'custom_comet_color': 'None'
     ,'custom_chase_color': 'aqua'
     ,'multi_chase_color': ['red', 'green', 'gold', 'blue']
     ,'cycle_color': 'palette_rainbow'
+    ,'custom_pacman_color': 'None'
     ,'custom_solid_color': 'magenta'
     ,'motion_solid_color': 'old_lace'
-    ,'rainbow_sparkle_sparkles_override': 50
-    ,'random_sparkle_speed_override': 0.05
-    ,'rainbow_cycle_speed_override': 10
-    ,'cycle_speed_override': 8
-    ,'reset_lighting_timeout': 10
+    ,'custom_sparkle_sparkles_override': 'None'
+    ,'random_sparkle_sparkles_override': 'None'
+    ,'rainbow_chase_size_override': 'None'
+    ,'rainbow_chase_spacing_override': 'None'
+    ,'random_chase_size_override': 'None'
+    ,'random_chase_spacing_override': 'None'
+    ,'custom_chase_size_override': 'None'
+    ,'custom_chase_spacing_override': 'None'
+    ,'multi_comet_tail_length_override': 200
+    ,'custom_comet_tail_length_override': 'None'
+    ,'random_comet_tail_length_override': 'None'
+    ,'rainbow_comet_tail_length_override': 'None'
+    ,'cycle_speed_override': 'None'
+    ,'rainbow_cycle_speed_override': 'None'
+    ,'custom_blink_speed_override': 'None'
+    ,'random_blink_speed_override': 'None'
 }
